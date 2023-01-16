f8ad837a1f8443108e9912caab94a2c02327b18f
advanc3dUA <fash1k@gmail.com>
2023-01-16 11:13:31 +0100

    Initial commit

---
+++ ExampleApp-PhotoDemo/PhotoCell.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoCell.swift
+//  ExampleApp-PhotoDemo
+//
+//  Created by Yuriy Gudimov on 16.01.2023.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+}
---
+++ ExampleApp-PhotoDemo/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  ExampleApp-PhotoDemo
+//
+//  Created by Yuriy Gudimov on 16.01.2023.
+//
+
+import UIKit
+import Photos
+import Combine
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    private var cancellables: Set<AnyCancellable> = []
+    private var dataSource: UICollectionViewDiffableDataSource<Int, UIImage>!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        dataSource = UICollectionViewDiffableDataSource(collectionView: collectionView, cellProvider: { collectionView, indexPath, image in
+            
+            let photoCell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath) as! PhotoCell
+            photoCell.imageView.image = image
+            return photoCell
+        })
+    }
+
+    private func fetchPhotos() {
+        let options = PHFetchOptions()
+        options.fetchLimit = 50
+        let fetchResult = PHAsset.fetchAssets(with: options)
+        print("Fetched \(fetchResult.count) photos")
+        
+        let targetSize = CGSize(width: 512, height: 512)
+    }
+
+    @IBAction func loadPhotosTapped(_ sender: UIBarButtonItem) {
+    }
+    
+    private func imagePublisher(asset: PHAsset, targetSize: CGSize, contentMode: PHImageContentMode) -> AnyPublisher<UIImage?, Never> {
+        let requestOptions = PHImageRequestOptions()
+        requestOptions.isNetworkAccessAllowed = true
+        requestOptions.deliveryMode = .fastFormat
+        requestOptions.resizeMode = .exact
+        
+        fatalError()
+    }
+}
